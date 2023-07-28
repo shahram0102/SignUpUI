a0847ac26a018d9e6a673d7b32f83ed9f8a4db93
shahram <shahramkhaleh2001@gmail.com>
2023-07-28 22:12:22 +0330

    feat: complete home screen

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,7 +1,11 @@
-import { View } from "react-native";
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home() {
   return (
-    <View></View>
-  )
+    <SafeAreaView>
+      <Text>Home Screen</Text>
+    </SafeAreaView>
+  );
 }
