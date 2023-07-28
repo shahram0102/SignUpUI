b154ab45061bb64df1d5da9e8c48abace76fb71f
shahram <shahramkhaleh2001@gmail.com>
2023-07-28 22:06:21 +0330

    feat: complete welcome screen

--- screens/Welcome/Welcome.tsx
+++ screens/Welcome/Welcome.tsx
@@ -1,5 +1,53 @@
-import { View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { themeColors } from "../../theme";
 
 export default function Welcome() {
-  return <View></View>;
+  const navigation = useNavigation();
+
+  function onPressSignUp (){
+    navigation.navigate("SignUp")
+  }
+
+  function onPressLogin (){
+    navigation.navigate("Login")
+  }
+
+  return (
+    <SafeAreaView
+      className="flex-1"
+      style={{ backgroundColor: themeColors.bg }}
+    >
+      <View className="flex-1 flex justify-around my-4">
+        <Text className="text-white font-bold text-4xl text-center">
+          Let's Get Started!
+        </Text>
+        <View className="flex-row justify-center">
+          <Image
+            source={require("../../assets/images/welcome.png")}
+            style={{ width: 350, height: 350 }}
+          />
+        </View>
+        <View className="space-y-4">
+          <TouchableOpacity
+            onPress={onPressSignUp}
+            className="py-3 bg-yellow-400 mx-7 rounded-xl"
+          >
+            <Text className="text-xl font-bold text-center text-gray-700">
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+          <View className="flex-row justify-center">
+            <Text className="text-white font-semibold">
+              Already have an account?
+            </Text>
+            <TouchableOpacity onPress={onPressLogin}>
+              <Text className="font-semibold text-yellow-400"> Log In</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
